69e1bfa785d1cf3468bea78162335c4f29452b9a
Tomasz Maciejewski <ponton@jabster.pl>
2012-12-16 13:51:18 +0100

    uniform: choose source in the middle of the area

--- src/uniform.cpp
+++ src/uniform.cpp
@@ -17,7 +17,21 @@ void uniform_model::generate(unsigned n, double size)
         add_node(node(x, y));
     }
 
-    set_source(std::rand() % get_nodes().size());
+    node middle_node(size / 2, size / 2);
+    uid new_source = 0;
+    double best_dist = middle_node - get_nodes()[new_source];
+
+    for (uid u = 0; u < get_nodes().size(); u++)
+    {
+        double dist = middle_node - get_nodes()[u];
+        if (dist < best_dist)
+        {
+            best_dist = dist;
+            new_source = u;
+        } 
+    }
+
+    set_source(new_source);
 }
 
 }
